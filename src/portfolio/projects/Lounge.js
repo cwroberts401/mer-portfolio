ce439d92265edb0427ab4b67ba1def9eed2789f5
Carl Roberts <cwroberts401@gmail.com>
2023-03-18 17:47:32 -0400

    added to the lounge page

--- src/portfolio/projects/Lounge.js
+++ src/portfolio/projects/Lounge.js
@@ -1,8 +1,32 @@
 import React from "react";
+import { useState } from "react";
+
+const photos = [{photoURL:'lounge-01.jpg', alt:'lounge collection design', id:'1'},{photoURL:'lounge-02.jpg', alt:'lounge collection design', id:'2'},{photoURL:'lounge-03.jpg', alt:'lounge collection design', id:'3'},{photoURL:'lounge-04.jpg', alt:'lounge collection design', id:'4'},{photoURL:'lounge-05.jpg', alt:'lounge collection design', id:'5'},{photoURL:'lounge-06.jpg', alt:'lounge collection design', id:'6'},{photoURL:'lounge-07.jpg', alt:'lounge collection design', id:'7'},{photoURL:'lounge-08.jpg', alt:'lounge collection design', id:'8'},]
 
 function Lounge() {
+    const [enlarge, setEnlarge] = useState(null)
+
+    const handleClick = (e) => {
+        
+        setEnlarge(enlarge === null || enlarge !== e.target.id? e.target.id: null)
+    }
+
+    const listImages = photos.map(photo => 
+    <div key={photo.id} className={`transition-all ${enlarge === photo.id ?'col-span-8':'col-span-4'}`}>
+        <img className='' src={process.env.PUBLIC_URL + '/lounge/' + photo.photoURL} alt={photo.alt}/>
+        <button id={photo.id} onClick={handleClick} className={`ml-6 ${enlarge === '0'?'rotate-45':'rotate-0'}`}>+</button>
+    </div>
+    
+    );
+
     return (
-        <p>this is the lounge project page</p>
+        <div className='justify-center items-center grid xl:grid-col-12 md:grid-cols-8 grid-cols-4 md:gap-4 md:mx-20 gap-4 mx-4 my-10'>
+            <div className='col-span-8 text-center'>
+                <h2 className='text-2xl'>Loungewear Collection for Lively</h2>
+                <p className='text-sm'> this is the longer description that can go below explaing the project </p>
+            </div>
+            {listImages}
+        </div>
     )
 }
 
--- src/portfolio/projects/Modal.js
+++ src/portfolio/projects/Modal.js
@@ -12,26 +12,19 @@ function Modal() {
 
 
     return (
-        <div className='grid xl:grid-col-12 md:grid-cols-8 grid-cols-4 md:gap-4 md:mx-20 gap-4 mx-4 my-10 items-center'>
-            <div className='col-span-3'>
-                <h2 className='text-2xl'>this is the title of the page</h2>
-                <p> this is the longer description that can go below explaing the project </p>
+        <div className='justify-center items-center grid xl:grid-col-12 md:grid-cols-8 grid-cols-4 md:gap-4 md:mx-20 gap-4 mx-4 my-10'>
+            <div className='col-span-8 text-center'>
+                <h2 className='text-2xl'>Modal Collection for Lively</h2>
+                <p className='text-sm'> this is the longer description that can go below explaing the project </p>
             </div>
-            <div className={`transition-all ${enlarge === '0' ?'col-span-8':'col-span-5'}`}>
-                <img className='' src={process.env.PUBLIC_URL + '/modal/00.jpg'} alt='Crochet Lace Campaign'/>
-                <button id='0' onClick={handleClick} className={`${enlarge === '0'?'rotate-45':'rotate-0'}`}>+</button>
+            <div className={`relative transition-all ${enlarge === '0' ?'col-span-8':'col-start-2 col-span-6'}`}>
+                <img className='' src={process.env.PUBLIC_URL + '/modal/modal-01.jpg'} alt='Crochet Lace Campaign'/>
+                <button id='0' onClick={handleClick} className={`text-white text-xl absolute bottom-10 left-4 ${enlarge === '0'?'rotate-45':'rotate-0'}`}>+</button>
             </div>
-            <div className={`${enlarge === '1' ?'col-span-8':'col-span-5'}`}>
-                 <img className='' src={process.env.PUBLIC_URL + '/modal/01.png'} alt='Crochet Lace Campaign'/>
+            <div className={`${enlarge === '1' ?'col-span-8':'col-start-2 col-span-6'}`}>
+                 <img className='' src={process.env.PUBLIC_URL + '/modal/modal-02.jpg'} alt='Crochet Lace Campaign'/>
                  <button id='1' onClick={handleClick} className={`${enlarge === '1'?'rotate-45':'rotate-0'}`}>+</button>
             </div>
-            <img className='col-span-3 object-cover w-full' src={process.env.PUBLIC_URL + '/modal/03.png'} alt='Crochet Lace Campaign'/>
-            <img className='col-span-2 object-cover w-full' src={process.env.PUBLIC_URL + '/modal/04.png'} alt='Crochet Lace Campaign'/>
-            <div className={`${enlarge === '2'?'col-span-8':'col-span-6'}`}>
-                 <img className='' src={process.env.PUBLIC_URL + '/modal/02.png'} alt='Crochet Lace Campaign'/>
-                 <button id='2' onClick={handleClick} className={`${enlarge === '2'?'rotate-45':'rotate-0'}`}>+</button>
-            </div>
-            <img className='col-span-2 object-cover w-full' src={process.env.PUBLIC_URL + '/modal/05.jpg'} alt='Crochet Lace Campaign'/>
         </div>
     )
 }
