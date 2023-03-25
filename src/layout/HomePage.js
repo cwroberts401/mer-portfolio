b1b48fd464c48c7b942c412bc6ff6ac522b6b1f2
Carl Roberts <cwroberts401@gmail.com>
2023-03-25 08:43:35 -0400

    changes to homepage

--- src/layout/HomePage.js
+++ src/layout/HomePage.js
@@ -4,7 +4,7 @@ export default function HomePage() {
 
     return (
         <div>
-            <img className='w-full' src={process.env.PUBLIC_URL + '/img/moood.png'} alt='Modal Campaign'/>
+            <img className='w-full h-full' src={process.env.PUBLIC_URL + '/img/moood.png'} alt='Modal Campaign'/>
         </div>
     )
 }
